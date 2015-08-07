151ef5b4267840e548af0509bdce3e174d8031a3
Berlin Patience <starsberlin@gmail.com>
2015-08-07 04:10:19 -0400

    change root path, remove some stuff in update page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :photos
   devise_for :users
-  root 'pages#home'
+  root 'photos#index'
   get 'about'=> 'pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
